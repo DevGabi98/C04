ea5172c659c23a99160cade85d6903a997016c5a
Viorica Gabriela Hacman <vhacman@e3r5p3.42roma.it>
2024-11-25 19:02:25 +0100

    ok

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vhacman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:48:29 by vhacman           #+#    #+#             */
-/*   Updated: 2024/11/25 17:04:25 by vhacman          ###   ########.fr       */
+/*   Updated: 2024/11/25 19:00:34 by vhacman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,26 @@ int	ft_atoi(char *str)
 	result = 0;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-')
+	while (str[i] == '+' || str[i] == '-')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
-	while (str[i] != '\0' && str[i] >= '0' && str[i] <= 9)
+	while (str[i] >= 48 && str[i] <= 57)
 	{
-		result = result * 10;
-		result += str[i] - '0';
+		result = result * 10 + (str[i] - '0');
 		i++;
 	}
 	return (result * sign);
 }
 /*
-int main(void)
+int main(int argc, char **argv)
 {
-    char str1[] = "   -1234";
-    char str2[] = "42";
-    char str3[] = "   +5678";
-    char str4[] = "abcd123";
-    char str5[] = "123abcd";
-
-    printf("Input: '%s', Output: %d\n", str1, ft_atoi(str1)); // Expected: -1234
-    printf("Input: '%s', Output: %d\n", str2, ft_atoi(str2)); // Expected: 42
-    printf("Input: '%s', Output: %d\n", str3, ft_atoi(str3)); // Expected: 5678
-    printf("Input: '%s', Output: %d\n", str4, ft_atoi(str4)); // Expected: 0
-    printf("Input: '%s', Output: %d\n", str5, ft_atoi(str5)); // Expected: 123
-
-    return 0;
+	if (argc == 2)
+	{
+		int	result = ft_atoi(argv[1]);
+		printf("The integer value is: %d\n", result);
+	}
+	return 0;
 }*/
